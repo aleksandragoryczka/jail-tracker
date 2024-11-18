8679f02f9b90749c51a6558a0eb90c11770a061f
krzysiek <krzysiek.kolodziejczyk@vp.pl>
2024-11-18 18:39:59 +0100

    Added dockerization for backend

--- backend/JailTracker/JailTracker.Api/Program.cs
+++ backend/JailTracker/JailTracker.Api/Program.cs
@@ -17,12 +17,8 @@ var app = builder.Build();
 
 Configure(app);
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
