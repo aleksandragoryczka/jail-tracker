a46a68743854fdf6f44637dbccd57ed8e95aea90
Ivan Filipchuk <ivanflpck@gmial.com>
2024-11-19 15:53:24 +0100

    Connected backend and frontend

--- backend/JailTracker/JailTracker.Api/Program.cs
+++ backend/JailTracker/JailTracker.Api/Program.cs
@@ -1,4 +1,4 @@
-using JailTracker.Api.Extensions;
+﻿using JailTracker.Api.Extensions;
 using JailTracker.Database;
 using Microsoft.EntityFrameworkCore;
 
@@ -6,27 +6,48 @@ var builder = WebApplication.CreateBuilder(args);
 var config = builder.Configuration;
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 builder.Services.AddCustomDbContext(config);
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var corsPolicy = "_myAllowSpecificOrigins";
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("_myAllowSpecificOrigins", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
 
 var app = builder.Build();
 
 Configure(app);
 
-app.UseSwagger();
-app.UseSwaggerUI();
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();
+
+app.UseCors(corsPolicy);
 
 app.UseAuthorization();
 
 app.MapControllers();
 
 app.Run();
+
 static void Configure(IApplicationBuilder app)
 {
     using var serviceScope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope();
